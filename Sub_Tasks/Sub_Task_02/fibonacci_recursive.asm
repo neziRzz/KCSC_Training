ee86d22cf15a7cd2353c4f3887f1ed51db8fdd3d
neziRzz <126742756+neziRzz@users.noreply.github.com>
2024-10-08 22:54:23 +0700

    Update fibonacci_recursive.asm

--- Sub_Tasks/Sub_Task_02/fibonacci_recursive.asm
+++ Sub_Tasks/Sub_Task_02/fibonacci_recursive.asm
@@ -21,6 +21,7 @@ fib proc
     mov edi, ecx
     cmp edi, 2
     jge recur
+
     mov eax, edi
     pop edi
     ret
@@ -28,9 +29,11 @@ recur:
     push esi
     lea ecx, [edi-2]
     call fib 
+
     lea ecx, [edi-1]
     mov esi, eax
     call fib
+
     add eax, esi
     pop esi
     pop edi
@@ -43,12 +46,14 @@ start:
     push OFFSET num
     push OFFSET int_format
     call scanf
+
     add esp, 8
     mov esi, num
     cmp esi, 2
     jl final    
     lea ecx, [esi-2]
     call fib
+
     lea ecx, [esi-1]
     mov edx, eax
     call fib
@@ -57,6 +62,7 @@ final:
     push esi
     push OFFSET ms2
     call printf
+
     add esp, 8
     push 0
     call ExitProcess

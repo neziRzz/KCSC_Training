10d1404cde00c69a452ee66cdf4b578b5696b3d9
neziRzz <126742756+neziRzz@users.noreply.github.com>
2024-10-14 18:17:37 +0700

    Update RC4.asm

--- Sub_Tasks/Sub_Task_02/RC4.asm
+++ Sub_Tasks/Sub_Task_02/RC4.asm
@@ -1,7 +1,6 @@
-; Architecture: x86
-.386
+    .386
 .model flat, stdcall
-option casemap : none                
+option casemap : none
 include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc
 include \masm32\include\user32.inc
@@ -15,11 +14,10 @@ printf proto C, :VARARG
 scanf proto C, :VARARG
 
 .data
-    
     string_format db "%s",0
-    promptKey       db "Enter key(no_spaces_allowed): ", 0
-    promptPlaintext db 0Ah,"Enter plaintext(no_spaces_allowed): ", 0
-    encryptedPrompt db "Encrypted Data: ", 0
+    promptKey       db "Enter key (no spaces allowed): ", 0
+    promptPlaintext db 0Ah,"Enter plaintext (no spaces allowed): ", 0
+    encryptedPrompt db 0Ah,"Encrypted Data: ", 0
     cypher_format db "%02X ",0
     key byte 256 dup(0)
     plaintext byte 256 dup(0)
@@ -27,106 +25,126 @@ scanf proto C, :VARARG
     plaintext_len DWORD ?
     s_box byte 256 dup(0)
     t_box byte 256 dup(0)
-    
+
 .code
+
+RC4 proc
+    ; edi = key_len, esi = plaintext_len, plaintext XORed with s_box after KSA and PRNG
+    
+    xor eax, eax
+    mov edi, key_len       ; edi = key length
+    mov esi, plaintext_len ; esi = plaintext length
+    
+    xor ebx, ebx
+    xor ecx, ecx
+
+init_sbox_tbox:     ; initialize the S-box and T-box 
+    mov eax, ecx
+    mov [s_box+ecx], cl  
+    cdq                    
+    idiv edi              
+    mov al, [key+edx]      
+    mov [t_box+ecx], al
+    inc ecx
+    cmp ecx, 100h          
+    jnz init_sbox_tbox
+
+    xor edi, edi
+
+KSA: ; KSA step to randomize the S-box
+    mov dl, [edi+s_box]    
+    movzx eax, [edi+t_box] 
+    add ebx, eax           
+    movzx ecx, dl         
+    add ebx, ecx
+    and ebx, 800000FFh     
+    jns swap_values        
+    
+    dec ebx                
+    or ebx, 0FFFFFF00h
+    inc ebx
+
+swap_values:              ; swap S[i] and S[j]
+    mov al, [ebx+s_box]    
+    mov [ebx+s_box], dl    
+    mov [edi+s_box], al    
+    
+    inc edi
+    cmp edi, 100h          
+    jl KSA
+
+    xor edi, edi
+    xor edx, edx           
+    test esi, esi          
+    jle print_encrypted_data
+
+    xor ebx, ebx
+
+gen_keystream:       ; PRGA step: generate keystream
+    inc ebx
+    and ebx, 800000FFh     
+    jns calculate_indices  
+    
+    dec ebx                
+    or ebx, 0FFFFFF00h
+    inc ebx
+
+calculate_indices:        ; calculate the new indices for S-box swapping
+    mov cl, [ebx+s_box]    
+    movzx eax, cl
+    add edi, eax           
+    and edi, 800000FFh     
+    jns encrypt_byte       
+    
+    dec edi
+    or edi, 0FFFFFF00h
+    inc edi
+
+encrypt_byte:             ; xor plaintext with the keystream byte
+    mov al, [edi+s_box]   
+    mov [edi+s_box], cl    
+    mov [ebx+s_box], al    
+    movzx ecx, [edi+s_box] 
+    movzx eax, al
+    add ecx, eax           
+    movzx eax, cl
+    movzx eax, [eax+s_box] 
+    xor al, [edx+plaintext]
+    mov [edx+t_box], al   
+    inc edx
+    cmp edx, esi           
+    jl  gen_keystream
+    ret                   
+RC4 endp
+
 start:
+
     invoke printf, OFFSET promptKey
     invoke scanf, OFFSET string_format, OFFSET key
     invoke printf, OFFSET promptPlaintext
     invoke scanf, OFFSET string_format, OFFSET plaintext
+    
+
     invoke lstrlen, OFFSET plaintext
     mov plaintext_len, eax
     invoke lstrlen, OFFSET key
     mov key_len, eax
-    xor eax, eax
-    mov edi, key_len
-    mov esi, plaintext_len
     
-    xor ebx, ebx
-    xor ecx, ecx
-init_tbox_sbox:    
-    mov eax, ecx
-    mov [s_box+ecx], cl
-    cdq
-    idiv edi
-    mov al, [key+edx]
-    mov [t_box+ecx], al
-    inc ecx
-    cmp ecx, 100h
-    jnz init_tbox_sbox
-    
-    xor edi, edi
-KSA: ;init KSA
-    mov dl, [edi+s_box]
-    movzx eax, [edi+t_box]
-    add ebx, eax
-    movzx ecx, dl
-    add ebx, ecx
-    and ebx, 800000FFh
-    jns swap
-    
-    dec ebx
-    or ebx, 0FFFFFF00h
-    inc ebx
-swap:
-    mov al, [ebx+s_box]
-    mov [ebx+s_box], dl
-    mov [edi+s_box], al
-    inc edi
-    cmp edi, 100h
-    jl KSA
-    
-    xor edi, edi
-    xor edx, edx
-    test esi, esi
-    jle print_cyphertext
-    
-    xor ebx, ebx
-smthin: ;indices_handling
-    inc ebx
-    and ebx, 800000FFh
-    jns next_label
-    
-    dec ebx
-    or ebx, 0FFFFFF00h
-    inc ebx
-next_label: ;indices_handling   
-    mov cl, [ebx+s_box]
-    movzx eax, cl
-    add edi, eax
-    and edi, 800000FFh
-    jns encrypt_routine 
-    
-    dec edi
-    or  edi, 0FFFFFF00h
-    inc edi
-    
-encrypt_routine: ; PRNG
-    mov al, [edi+s_box]
-    mov [edi+s_box], cl
-    mov [ebx+s_box], al
-    movzx ecx, [edi+s_box]
-    movzx eax, al
-    add ecx, eax
-    movzx eax, cl
-    movzx eax, [eax+s_box]
-    xor al, [edx+plaintext]
-    mov [edx+t_box], al
-    inc edx
-    cmp edx, esi
-    jl  smthin
-    
-print_cyphertext:
+
+    call RC4
+
+print_encrypted_data:   
     invoke printf, OFFSET encryptedPrompt
     xor edi, edi
-
-iterate_cyphertext:
-    movzx eax, [edi+t_box]
-    invoke printf, OFFSET cypher_format, eax
+iterate_encrypted_data:
+    movzx eax, [edi+t_box] 
+    invoke printf, OFFSET cypher_format, eax 
     inc edi
-    cmp edi, esi
-    jl iterate_cyphertext
-    jmp exit
-exit:
-    invoke ExitProcess, 0
+    cmp edi, esi           
+    jl iterate_encrypted_data
+    
+    jmp exit_program
+
+exit_program:
+    invoke ExitProcess, 0  
     end start

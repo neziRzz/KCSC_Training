67413ae236eb2c0ab9ed35889c32037c52fc44dd
neziRzz <126742756+neziRzz@users.noreply.github.com>
2024-10-14 23:41:08 +0700

    reformatted

--- Sub_Tasks/Sub_Task_02/RC4.asm
+++ Sub_Tasks/Sub_Task_02/RC4.asm
@@ -38,83 +38,83 @@ RC4 proc
     xor ebx, ebx
     xor ecx, ecx
 
-init_sbox_tbox:     ; initialize the S-box and T-box 
-    mov eax, ecx
-    mov [s_box+ecx], cl  
-    cdq                    
-    idiv edi              
-    mov al, [key+edx]      
-    mov [t_box+ecx], al
-    inc ecx
-    cmp ecx, 100h          
-    jnz init_sbox_tbox
-
-    xor edi, edi
-
-KSA: ; KSA step to randomize the S-box
-    mov dl, [edi+s_box]    
-    movzx eax, [edi+t_box] 
-    add ebx, eax           
-    movzx ecx, dl         
-    add ebx, ecx
-    and ebx, 800000FFh     
-    jns swap_values        
+    init_sbox_tbox:     ; initialize the S-box and T-box 
+        mov eax, ecx
+        mov [s_box+ecx], cl  
+        cdq                    
+        idiv edi              
+        mov al, [key+edx]      
+        mov [t_box+ecx], al
+        inc ecx
+        cmp ecx, 100h          
+        jnz init_sbox_tbox
     
-    dec ebx                
-    or ebx, 0FFFFFF00h
-    inc ebx
+        xor edi, edi
 
-swap_values:              ; swap S[i] and S[j]
-    mov al, [ebx+s_box]    
-    mov [ebx+s_box], dl    
-    mov [edi+s_box], al    
-    
-    inc edi
-    cmp edi, 100h          
-    jl KSA
+        KSA: ; KSA step to randomize the S-box
+            mov dl, [edi+s_box]    
+            movzx eax, [edi+t_box] 
+            add ebx, eax           
+            movzx ecx, dl         
+            add ebx, ecx
+            and ebx, 800000FFh     
+            jns swap_values        
+            
+            dec ebx                
+            or ebx, 0FFFFFF00h
+            inc ebx
 
-    xor edi, edi
-    xor edx, edx           
-    test esi, esi          
-    jle print_encrypted_data
+            swap_values:              ; swap S[i] and S[j]
+                mov al, [ebx+s_box]    
+                mov [ebx+s_box], dl    
+                mov [edi+s_box], al    
+                
+                inc edi
+                cmp edi, 100h          
+                jl KSA
+            
+                xor edi, edi
+                xor edx, edx           
+                test esi, esi          
+                jle print_encrypted_data
+            
+                xor ebx, ebx
 
-    xor ebx, ebx
+                gen_keystream:       ; PRGA step: generate keystream
+                    inc ebx
+                    and ebx, 800000FFh     
+                    jns calculate_indices  
+                    
+                    dec ebx                
+                    or ebx, 0FFFFFF00h
+                    inc ebx
 
-gen_keystream:       ; PRGA step: generate keystream
-    inc ebx
-    and ebx, 800000FFh     
-    jns calculate_indices  
-    
-    dec ebx                
-    or ebx, 0FFFFFF00h
-    inc ebx
+                    calculate_indices:        ; calculate the new indices for S-box swapping
+                        mov cl, [ebx+s_box]    
+                        movzx eax, cl
+                        add edi, eax           
+                        and edi, 800000FFh     
+                        jns encrypt_byte       
+                        
+                        dec edi
+                        or edi, 0FFFFFF00h
+                        inc edi
 
-calculate_indices:        ; calculate the new indices for S-box swapping
-    mov cl, [ebx+s_box]    
-    movzx eax, cl
-    add edi, eax           
-    and edi, 800000FFh     
-    jns encrypt_byte       
-    
-    dec edi
-    or edi, 0FFFFFF00h
-    inc edi
-
-encrypt_byte:             ; xor plaintext with the keystream byte
-    mov al, [edi+s_box]   
-    mov [edi+s_box], cl    
-    mov [ebx+s_box], al    
-    movzx ecx, [edi+s_box] 
-    movzx eax, al
-    add ecx, eax           
-    movzx eax, cl
-    movzx eax, [eax+s_box] 
-    xor al, [edx+plaintext]
-    mov [edx+t_box], al   
-    inc edx
-    cmp edx, esi           
-    jl  gen_keystream
-    ret                   
+                        encrypt_byte:             ; xor plaintext with the keystream byte
+                            mov al, [edi+s_box]   
+                            mov [edi+s_box], cl    
+                            mov [ebx+s_box], al    
+                            movzx ecx, [edi+s_box] 
+                            movzx eax, al
+                            add ecx, eax           
+                            movzx eax, cl
+                            movzx eax, [eax+s_box] 
+                            xor al, [edx+plaintext]
+                            mov [edx+t_box], al   
+                            inc edx
+                            cmp edx, esi           
+                            jl  gen_keystream
+                            ret                   
 RC4 endp
 
 start:
